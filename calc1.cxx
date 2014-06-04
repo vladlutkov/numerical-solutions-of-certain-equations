9769b1aedb273243c94340553eac89d44e83f7f6
Vladislav Lutkov <v.lutkov@ideco.ru>
2014-06-04 19:44:54 +0600

    dev

--- calc1.cxx
+++ calc1.cxx
@@ -1,7 +1,16 @@
-#include <stdio.h>
-#include <math.h>
-#include <list.h>
+#include <iostream>
+#include <cmath>
+#include <list>
 
+double func(double t, double x) {
+	return 2*t + x + cos(t);
+}
+double nu(double t, double x) {
+	return 0;	
+}
+double gu(double t, double x) {
+	return 0;	
+}
 
 int main()
 {
@@ -12,26 +21,25 @@ int main()
 	int xcount = 10;
 	int tcount = 10;
 
-	double xh = abs(tn - x0) / xcount;
-	double th = abs(tn - t0) / tcount;
+	double xh = std::abs(tn - x0) / xcount;
+	double th = std::abs(tn - t0) / tcount;
 
-	list <double[3]> result;
+	std::list <double> u;
+	std::list <double> x;
+	std::list <double> t;
+
 	for(double x = x0; x <= xn; x += xh){
 		for(double t = t0; t <= tn; t += th) {
-			double[3] res = {x, t, func(x, t)};
-			list.push_back(res);
-			printf("(%f, %f, %f)\n", res[0], res[1], res[2]);
+			u.push_back(func(x, t));
+			x.push_back(x);
+			t.push_back(t);
 		}
+	}
+
+	
+	for(int i = 0; i < u.size(); i++) {
+		std::cout << "(" << t.pop_back() << ", " << x.pop_back() << ", " << u.pop_back() << ")";
 	}
 	
 	return 0;
-}
-double func(t, x) {
-	return 2*t + x + cos(t);
-}
-double nu(t, x) {
-	return 0;	
-}
-double gu(t, x) {
-	return 0;	
 }
--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
-filename = "calc1.cxx";
+filename = "calc1";
 
 log = function(str) {
   console.log("### Service Action ###");
@@ -6,51 +6,46 @@ log = function(str) {
   return console.log("######################");
 };
 
-postData = function(formula) {
-  var spawn = require("child_process").spawn;
+var spawn = require("child_process").spawn;
 
-  var scp = spawn("scp", ["calc.cxx", "s0051@umt.imm.uran.ru:~/"]);
-  var ssh = spawn("ssh", ["-tt", "s0051@umt.imm.uran.ru"]);
+var scp = spawn("scp", [filename + ".cxx", "s0051@umt.imm.uran.ru:~/"]);
+var ssh = spawn("ssh", ["-tt", "s0051@umt.imm.uran.ru"]);
 
-  var commands = {
-    prepare: [
-      "mpicxx -o calc calc.cxx"
-    ],
-    execute: [
-      "./calc"
-    ],
-    terminate: [
-      "exit",
-      "rm calc calc.cxx"
-    ]
-  };
+var commands = {
+  prepare: [
+  "mpicxx -o " + filename + " " + filename + ".cxx"
+  ],
+  execute: [
+  "./" + filename
+  ],
+  terminate: [
+  "exit",
+  "rm " + filename + " " + filename + ".cxx"
+  ]
+};
 
-  ssh.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
+ssh.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
 
-    if (("" + data).indexOf("[s0051@umt ~]$") == -1) return;
+  if (("" + data).indexOf("[s0051@umt ~]$") == -1) return;
 
-    if (commands.prepare.length != 0) {
-      console.log('===prepare===');
-      ssh.stdin.write(commands.prepare.pop() + "\r\n");
-      return;
-    }
-    if (commands.execute.length != 0) {
-      console.log('===execute===');
-      ssh.stdin.write(commands.execute.pop() + "\r\n");
-      return;
-    }
-    if (commands.terminate.length != 0) {
-      console.log('===terminate===');
-      ssh.stdin.write(commands.terminate.pop() + "\r\n");
-      return;
-    }
-  });
+  if (commands.prepare.length != 0) {
+    console.log('===prepare===');
+    ssh.stdin.write(commands.prepare.pop() + "\r\n");
+    return;
+  }
+  if (commands.execute.length != 0) {
+    console.log('===execute===');
+    ssh.stdin.write(commands.execute.pop() + "\r\n");
+    return;
+  }
+  if (commands.terminate.length != 0) {
+    console.log('===terminate===');
+    ssh.stdin.write(commands.terminate.pop() + "\r\n");
+    return;
+  }
+});
 
-  ssh.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
-}
-
-
-postData()
+ssh.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
